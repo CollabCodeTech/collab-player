150e2eee55e8e65c66bc76340163ddd14510de82
Marco Bruno <marco.bruno.br@gmail.com>
2020-01-30 00:31:49 +0100

    :art: Remove serviceWorker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Vai Brasil!!!!</h1>;
 }
 
 export default App;

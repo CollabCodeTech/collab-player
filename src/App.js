87116180c5db17668c29656912650f2d604f1b6d
Marco Bruno <marco.bruno.br@gmail.com>
2020-01-30 01:05:48 +0100

    :construction: Create PlayerVideo component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import PlayerVideo from "./components/PlayerVideo";
 
 function App() {
-  return <h1>Vai Brasil!!!!</h1>;
+  return <PlayerVideo />;
 }
 
 export default App;
---
+++ src/components/PlayerVideo/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PlayerVideo() {
+  return (
+    <video src="http://sveltejs.github.io/assets/caminandes-llamigos.mp4"></video>
+  );
+}
+
+export default PlayerVideo;
